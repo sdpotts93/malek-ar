9138820e901c5e3ce481704a4bae854aa2ef8306
Steven Potts <stevenpotts@Stevens-MacBook-Pro.local>
2023-07-21 07:58:07 -0600

    camera working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a href="intent://arvr.google.com/scene-viewer/1.0?mode=ar_preferred&disable_occlusion=true&enable_vertical_placement=true&file=https%3A%2F%2Fmy-personal-assets.s3.us-west-1.amazonaws.com%2Fmatisse.gltf#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https%3A%2F%2Fwww.studiomalek.com%2F%23model-viewer-no-ar-fallback;end">
+    <a href="intent://arvr.google.com/scene-viewer/1.0?mode=ar_preferred&disable_occlusion=true&enable_vertical_placement=true&file=https%3A%2F%2Fs3.eu-west-2.amazonaws.com%2Fproduct.baetes.com%2FP7650933_Tiger_30x40_WEBB.glb#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https%3A%2F%2Far.baetes.com%2FP7650933_Tiger_30x40_WEBB%3F%26text%3DTESTA%2BI%2BHEMMAMILJ%25c3%2596%23model-viewer-no-ar-fallback;end">
       <button>
         BUTTON
       </button>
